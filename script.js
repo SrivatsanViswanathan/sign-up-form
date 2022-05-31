c0c812793ba629af123d93d18a79b8bdd38ad163
Srivatsan Viswanathan <sriv.viswanathan@gmail.com>
2022-05-31 15:23:30 -0400

    Validation Error fixed

--- script.js
+++ script.js
@@ -10,25 +10,31 @@ function validate() {
     const confirmPassword = document.getElementById('confirmPassword');
     const error = document.getElementById('error');
     const cError = document.getElementById('cError');
+    let mismatch = true;
+    let valid = false;
+    console.log(mismatch);
     if (password.value === confirmPassword.value) {
+        mismatch = false;
         cError.textContent = ''
         cError.style.opacity = '0';
         password.style.borderColor = 'gray';
         confirmPassword.style.borderColor = 'gray';
     }
-    else {
-        cError.textContent = '* Passwords do not match.'
-        cError.style.opacity = '1';
-        password.style.borderColor = 'red';
-        confirmPassword.style.borderColor = 'red';
-    }
-    if (number.test(password.value) && special.test(password.value) && letter.test(password.value) && password.value === confirmPassword.value) {
+    if (number.test(password.value) && special.test(password.value) && letter.test(password.value)) {
+        valid = true;
         error.textContent = ''
         error.style.opacity = '0';
         password.style.borderColor = 'gray';
         confirmPassword.style.borderColor = 'gray';
     }
-    else {
+
+    if (mismatch) {
+        cError.textContent = '* Passwords do not match.'
+        cError.style.opacity = '1';
+        password.style.borderColor = 'red';
+        confirmPassword.style.borderColor = 'red';
+    }
+    if (!valid) {
         error.textContent = '* Password must contain at least 1 number, letter, and special character'
         error.style.opacity = '1';
         password.style.borderColor = 'red';
